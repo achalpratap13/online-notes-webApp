b57733d7e072edff9667900aa2be9175b8211abe
Achal Pratap <pratapachal7@gmail.com>
2026-01-16 22:22:54 +0530

    Refactor MongoDB connection and simplify server setup

--- server.js
+++ server.js
@@ -3,24 +3,19 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
 const noteRoutes = require("./routes/noteRoutes");
 app.use("/api/notes", noteRoutes);
 
-// MongoDB connection
+// MongoDB Atlas
+const MONGO_URL = "mongodb+srv://honeych0013_db_user:honey_notes@cluster0.gr2ah8y.mongodb.net/notesDB?retryWrites=true&w=majority";
+
 mongoose
-  .connect(process.env.MONGO_URL)
+  .connect(MONGO_URL)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log("MongoDB error:", err));
 
-// IMPORTANT: Render PORT
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
